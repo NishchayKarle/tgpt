a276d9f056ead5a815e4f125b7cb5a6603ddcbf2
nishchaykarle <nishchaykarle@gmail.com>
2023-12-23 22:09:15 -0600

    update

--- tgpt.py
+++ tgpt.py
@@ -9,7 +9,6 @@ import asyncio
 client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 console = Console()
 
-
 async def send_request():
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
